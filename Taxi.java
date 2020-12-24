d8074c52e964373c718eb90e695b0eb59bb706e2
Start-debug <57304430+Start-debug@users.noreply.github.com>
2020-12-24 17:12:14 +0800

    Update on 2020/12/14
    The update changes some of the function for computing the fee.
    Such as:
    1)fee of driving in low speed
    2)fee of going on a highway

--- Taxi.java
+++ Taxi.java
@@ -2,6 +2,7 @@ import java.lang.Math;
 
 public class Taxi {
     public static void main(String[] args) {
+        System.out.println("====================================================");
         for (int i = 5; i <= 20; i = i + 5) {
             System.out.println("Long of this journey:" + i + "km");
             System.out.println("Start driving...");
@@ -12,23 +13,24 @@ public class Taxi {
                 fee = 2.2 * (i - 3) + 12 + 0.5 * 2.2 * (i - 6);
             }
             for (int j = 1; j <= i; j++) {
-                if ((int) Math.random() % 2 == 0) {
+                if ((int) Math.random() % 5 == 0) {
                     System.out.println("Ahhhhh!");
-                    int flag = (int) (Math.random() * 100000) % 10;
+                    int flag = (int) (Math.random() * 100000) % 15;
                     System.out.println("You drive in low speed for " + flag + " km");
                     if (flag <= 5)
                         fee = fee + flag * 0.4;
                     else {
                         fee = fee + (flag - 5) * 0.5 + 2;
+                    
                     }
                 }
                 if ((int) Math.random() % 7 == 0) {
                     System.out.println("Go on the highway...");
-                    System.out.println("You spend " + 10 + " yuan.");
+                    System.out.println("You spend " + (int)(Math.random()*1000000)%50 + " yuan.");
                 }
             }
-            System.out.println("You spend " + fee + " yuan for the journey.");
-            System.out.println("====================================================\n\n\n\n\n\n");
+            System.out.println("You spend " + (float)Math.round(fee*100)/100 + " yuan for the journey.");
+            System.out.println("====================================================");
         }
     }
 }
